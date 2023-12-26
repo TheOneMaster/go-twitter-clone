ced910bdeb83e33e5ffc8d12c41e91b534d3f0e9
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2023-12-26 15:20:38 +0100

    Correct variable name

--- db/Users.go
+++ db/Users.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func CheckUserExists(username string) bool {
 	var count int
-	err := CON.Get(&count, "SELECT count(*) FROM Users WHERE username==?", username)
+	err := Connection.Get(&count, "SELECT count(*) FROM Users WHERE username==?", username)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 	}
