b443a33a53a93cb1e7d0569fca094fff986aebc7
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2024-01-05 19:10:00 +0100

    Updated templates and template props

--- templates/types.go
+++ templates/types.go
@@ -1,10 +1,30 @@
 package templates
 
 type Message struct {
+	ID     int
 	Author string
 	Data   string
 	Time   string
 	Photo  string
+
+	// Metadata properties
+	Liked    bool
+	Selected bool
 }
 
 type MessageList []Message
+
+type User struct {
+	Username    string
+	DisplayName string
+	Photo       string
+}
+
+type Profile struct {
+	Username     string
+	DisplayName  string
+	DateCreated  string
+	ProfileImage string
+
+	Messages MessageList
+}
